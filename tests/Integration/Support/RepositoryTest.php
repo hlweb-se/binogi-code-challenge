616fdfb1cfe0e75e5b122baaaf390c4c40e5a65e
Calle Hellberg <calle.hellberg@oribi.se>
2023-08-31 17:36:36 +0200

    Additional tests added for create and update

--- tests/Integration/Support/RepositoryTest.php
+++ tests/Integration/Support/RepositoryTest.php
@@ -26,6 +26,28 @@ class RepositoryTest extends FrameworkTest
     {
         $user = $this->userFactory->create();
         $this->assertInstanceOf(User::class, $user);
+
+        // Make sure the new user has a nickname (not empty)
+        $this->assertNotEquals('', $user->refresh()->nickname);
+        $this->assertNotNull($user->refresh()->nickname);
+
+        // Make sure we can't create a user with a nickname that is too short
+        $response = $this->json('POST', '/api/users', [
+            "nickname" => "",
+            "name" => "Darth Maul",
+            "email" => "maul@redface.com",
+            "password" => "May The Force Be With You"
+        ]);
+        $response->assertStatus(422);
+
+        // Make sure we can't create a new user with a nickname that is too long
+        $response = $this->json('POST', '/api/users', [
+            "nickname" => "long-live-the-empire-and-all-of-our-stormtroopers-despite-their-poor-aim",
+            "name" => "Darth Maul",
+            "email" => "maul@redface.com",
+            "password" => "May The Force Be With You"
+        ]);
+        $response->assertStatus(422);
     }
 
     public function testUpdate()
@@ -36,9 +58,24 @@ class RepositoryTest extends FrameworkTest
         $this->repository->update(['name' => 'Luke Skywalker'], $user->id);
         $this->assertEquals('Luke Skywalker', $user->refresh()->name);
 
+        // Make sure that we can update a valid nickname
         $this->repository->update(['nickname' => 'jedi-master'], $user->id);
         $this->assertNotEquals('sith', $user->refresh()->nickname);
         $this->assertEquals('jedi-master', $user->refresh()->nickname);
+
+        // Make sure we can't update a nickname that is too short
+        $response = $this->json('PUT', '/api/users/' .$user->id, [
+            'nickname' => ''
+        ]);
+        $response->assertStatus(422);
+        $this->assertNotEquals('', $user->refresh()->nickname);
+
+        // Make sure we can't update a nickname that is too long
+        $response = $this->json('PUT', '/api/users/' .$user->id, [
+            'nickname' => 'drunk-jedi-with-a-name-that-is-too-long-to-even-remember-or-care-about'
+        ]);
+        $response->assertStatus(422);
+        $this->assertNotEquals('drunk-jedi-with-a-name-that-is-too-long-to-even-remember-or-care-about', $user->refresh()->nickname);
     }
 
     public function testDelete()
